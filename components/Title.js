46e0bee72bd84847163afca0a504b323ff2495fa
preciousyaks17 <johndoe@example.com>
2024-11-25 21:12:33 +0100

    chore: worked on displaying random numbers

---
+++ components/Title.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
+function Title({ children }) {
+  return (
+    <View>
+      <Text style={styles.title}>Opponent's Guess</Text>;
+    </View>
+  );
+}
+
+export default Title;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    color: Colors.accent500,
+    fontWeight: "bold",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 12,
+  },
+});
